42b9204dfb2e3eee61165c417099b67d470c3a3d
thekaroe-thailand <thekaroe@gmail.com>
2024-05-17 00:42:46 +0700

    add file code

---
+++ src/components/ControlSidebar.js
@@ -0,0 +1,8 @@
+function ControlSidebar() {
+    return <>
+        <aside class="control-sidebar control-sidebar-dark">
+        </aside>
+    </>
+}
+
+export default ControlSidebar;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return <>
+        <footer class="main-footer">
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            All rights reserved.
+            <div class="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.2.0
+            </div>
+        </footer>
+    </>
+}
+
+export default Footer;
